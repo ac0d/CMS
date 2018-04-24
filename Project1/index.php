1a82184e719fb2a6f287acbc93fe347e35747154
Leif Granfelt <leifgranfelt@gmail.com>
2018-04-24 11:28:37 +0000

    index login

--- Project1/index.php
+++ Project1/index.php
@@ -5,6 +5,14 @@
         <title>Get Benis</title>
     </head>
     <body>
-        
+        <form>
+            User name:<br>
+            <input type="text" name="usrname"><br>
+            Password:<br>
+            <input type="text" name="usrpwd"><br>
+            <input type="button" onclick="" value="SIGN IN"></button>
+            or
+            <input type="button" onclick="register.php" value="CREATE ACCOUNT"></button>
+</form>
     </body>
 </html>
