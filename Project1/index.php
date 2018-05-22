a8c5c3a41961674db9dd4a17a0f4f42e041a29b8
Leif Granfelt <leifgranfelt@gmail.com>
2018-05-22 13:32:05 +0000

    fixed forlders

--- Project1/index.php
+++ Project1/index.php
@@ -10,9 +10,36 @@
             <input type="text" name="usrname"><br>
             Password:<br>
             <input type="text" name="hashpass"><br>
-            <input type="button" onclick="" value="SIGN IN"></button>
+            <input type="button" onclick="location.href='profile.php'" value="SIGN IN"></button>
             or
-            <input type="button" onclick="register.php" value="CREATE ACCOUNT"></button>
+            <input type="button" onclick="location.href='register.php'" value="CREATE ACCOUNT"></button>
 </form>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+// Skapa uppkoppling
+$conn = new mysqli($servername, $username, $password);
+// Kolla att det går att koppla sig, om ej, skriv ut felet
+if ($conn->connect_error) {
+    die("Något gick snett: " . $conn->connect_error);
+}
+echo "Uppkopplingen till databasen lyckades</br>";
+
+$sql = "SELECT * FROM table_name WHERE some_criteria = some_value";
+
+$result = $db->query($sql);
+
+while($row = $result->fetch_assoc()){
+   echo $row['kolumn_namn'];
+}
+
+$sql = $db->prepare("SELECT * FROM users WHERE username = ?");
+$statement->bind_param('s', $name);
+
+$name = 'Bob';
+$stmt->execute();
+
+?>
     </body>
 </html>
