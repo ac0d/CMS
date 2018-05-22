f24a7069ed900c476e1741611e5974c1b13cc90a
Leif Granfelt <leifgranfelt@gmail.com>
2018-05-22 15:59:46 +0000

    added stuff

--- Project1/register.php
+++ Project1/register.php
@@ -36,49 +36,49 @@
             <input type="radio" name="group2" value="busy"> Bussy<br>
             <input type="radio" name="group2" value="both"> Both<br>
             </fieldset>
-             <button type="button">REGISTER!</button> 
+             <button type="button" onclick="register();alertfunction();location.href='profile.php'" value="REGISTER"></button> 
             
             <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "benis_users";
-            // Skapa uppkoppling
-            $conn = new mysqli($servername, $username, $password);
-            // Kolla att det går att koppla sig, om ej, skriv ut felet
-            if ($conn->connect_error) {
-                die("Något gick snett: " . $conn->connect_error);
+            function register(){
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "benis_users";
+                // Skapa uppkoppling
+                $conn = new mysqli($servername, $username, $password);
+                // Kolla att det går att koppla sig, om ej, skriv ut felet
+                if ($conn->connect_error) {
+                    die("Något gick snett: " . $conn->connect_error);
+                }
+                echo "Uppkopplingen till databasen lyckades</br>";
+                
+                $stmt = $conn->prepare("INSERT INTO users (usrname, hashpass, firstname, lastname, mail, zip, info, salary, ihave, lookingfor)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $stmt->bind_param("sssssisdss", $username, $hashpass, $firstname, $lastname, $mail, $zip, $info, $salary, $ihave, $lookingfor);
+                
+                $usrname = $_GET["username"];
+                $hashpass = md5($_GET["password"]);
+                $firstname = $_GET["firstname"];
+                $lastname =  $_GET["lastname"];
+                $mail = $_GET["email"];
+                $zip = $_GET["postalcode"];
+                $info = $_GET["about"];
+                $salary = $_GET["arslon"];
+                $ihave = $_GET["group1"];
+                $lookingfor = $_GET["group2"];
+                $stmt->execute();
+                
+                /*
+                $sql = "INSERT INTO users (usrname, hashpass, firstname, lastname, mail, zip, info, salary, ihave, lookingfor)
+                VALUES ($username, $hashpass,  $firstname, $lastname, $mail, $zip, $info, $salary, $ihave, $lookingfor)";
+                */
             }
-            echo "Uppkopplingen till databasen lyckades</br>";
-            
-            $usrname = $_GET["username"];
-            $hashpass = md5($_GET["password"]);
-            $firstname = $_GET["firstname"];
-            $lastname =  $_GET["lastname"];
-            $mail = $_GET["email"];
-            $zip = $_GET["postalcode"];
-            $info = $_GET["about"];
-            $salary = $_GET["arslon"];
-            $ihave = $_GET["group1"];
-            $lookingfor = $_GET["group2"];
-            
-            $sql = "INSERT INTO users (usrname, hashpass, firstname, lastname, mail, zip, info, salary, ihave, lookingfor)
-            VALUES ($username, $hashpass,  $firstname, $lastname, $mail, $zip, $info, $salary, $ihave, $lookingfor)";
-            
-            $result = $db->query($sql);
-            
-            while($row = $result->fetch_assoc()){
-               echo $row['kolumn_namn'];
-            }
-            
-            $sql = $db->prepare("SELECT * FROM users WHERE username = ?");
-            $statement->bind_param('s', $name);
-            
-            $name = 'Bob';
-            $stmt->execute();
-            
             ?>
-            
+            <script>
+            function alertfunction() {
+                alert("Account creation succesful!");
+            }
+            </script>
         </form>
     </body>
 </html>
