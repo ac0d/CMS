3af8f33aac422c7e465964d18120f3f075e207e6
Leif Granfelt <leifgranfelt@gmail.com>
2018-05-24 06:15:47 +0000

    limited view by 5 per page

--- Project1/index.php
+++ Project1/index.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 require_once('connect.php');
-$sql = "SELECT email, firstname, lastname, zip, salary, info, ihave, iwant FROM users";
-$result = mysqli_query($connection, $sql);
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +10,18 @@ $result = mysqli_query($connection, $sql);
         <title>Get Benis</title>
     </head>
     <body>
+        <h2>Benis dating site</h2><br>
         <?php
+        if(isset($_GET["page"])){
+            $page = $_GET["page"];
+        }else{
+            $page=1;
+        }
+        $limit = 5;
+        $offset = ($page-1) * $limit;
+        echo "Page ".$page.".<br><br>";
+        $sql = "SELECT email, firstname, lastname, zip, salary, info, ihave, iwant FROM users LIMIT $offset, $limit";
+        $result = mysqli_query($connection, $sql);
         if(isset($_SESSION['username'])){
             echo "You are logged in as: " . $_SESSION['username'] . ".<br><br>";
             if($result->num_rows > 0) {
@@ -24,7 +33,7 @@ $result = mysqli_query($connection, $sql);
                 echo "0 results";
             }
         }else{
-            echo "You are not logged in.<br><br>";
+            echo "You are not logged in. User e-mails hidden.<br><br>";
             if($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                 echo "Name: ".$row["firstname"]." ".$row["lastname"]." | Postal code: ".$row["zip"]." | Anual salary: ".$row["salary"]."€".
@@ -35,8 +44,23 @@ $result = mysqli_query($connection, $sql);
             }
         }
         ?>
+        <?php 
+        $sql = "SELECT COUNT(ID) AS total FROM users";
+        $result = mysqli_query($connection, $sql);
+        $row = $result->fetch_assoc();
+        $total_pages = ceil($row["total"] / $limit);
+        echo "Pages: ";  
+        for ($i=1; $i<=$total_pages; $i++)
+            {
+                echo "<a href='index.php?page=".$i."'";
+                if ($i==$page)
+                echo " class='curPage'";
+                echo ">".$i."</a> "; 
+            }; 
+        ?>
+        <br><br>
         <input type="button" onclick="location.href='login.php';" value="Login" />
         <input type="button" onclick="location.href='register.php';" value="Register" />
-        <input type="button" onclick="location.href='logout.php';" value="Logout" />
+        <input type="button" onclick="location.href='logout.php';" value="Logout" /><br><br>
     </body>
 </html>
